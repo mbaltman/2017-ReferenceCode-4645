c0de5bccb445a76a42aa8d37c5568e23a6022fb9
StefanBorjan <sborjan@cps.edu>
2017-02-16 16:01:01 -0600

    Update OI.java

--- Steamworks/src/org/usfirst/frc/team4645/robot/OI.java
+++ Steamworks/src/org/usfirst/frc/team4645/robot/OI.java
@@ -10,7 +10,8 @@ import org.usfirst.frc.team4645.robot.commands.*;
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-public class OI {
+public class OI 
+{
 	
 	public static Joystick leftJoy = new Joystick(1);
 	Button buttonShoot = new JoystickButton(leftJoy, 1);
@@ -27,17 +28,14 @@ public class OI {
 	
 	public static Button resetGyro = new JoystickButton(leftJoy, 7);
 	
-	public OI(){
+	public OI()
+	{
 		
 		tempXMag = -leftJoy.getX();
 		tempYMag = leftJoy.getY();
 		tempZMag = leftJoy.getZ();
 		
-		if (tempZMag < 0.2 && tempZMag > -0.2) {
-			tempZMag = 0;
-		}
-		if (tempZMag < 0) {tempZMag += 0.2;}
-		else if (tempZMag > 0) {tempZMag -= 0.2;}
+		
 		
 		resetGyro.whenPressed(new ResetGyro());
 
